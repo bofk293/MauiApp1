9acd02842facd58fe62a0db38d88f5c6e00ad6e0
bofk293 <bofk293@gmail.com>
2026-05-02 02:44:53 -0400

    Add files via upload

---
+++ x/file1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiApp1
+{
+    internal class file1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello..");
+            Console.Read();
+            Entry[] entries;
+            { entries = new Entry[1]; }
+            // config error : ambiguous.. 
+        }
+
+    }
+
+    internal class Entry
+    {
+        public Entry()
+        {
+            // Constructor logic here
+        }
+    }
+
+    public class Entry2
+    {
+
+
+    }
+
+    // entry block
+
+
+}
+
+static void HelperMain(string[] args)
+{
+    Console.Beep();
+
+}
